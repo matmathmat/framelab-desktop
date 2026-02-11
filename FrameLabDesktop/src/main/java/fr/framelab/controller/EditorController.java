14ab0d5c76a5c929404e8f303dd780f3a0784eaf
matmathmat <232357120+matmathmat@users.noreply.github.com>
2026-02-11 17:36:34 +0100

    L'image prend la taille du panel

--- FrameLabDesktop/src/main/java/fr/framelab/controller/EditorController.java
+++ FrameLabDesktop/src/main/java/fr/framelab/controller/EditorController.java
@@ -1,6 +1,7 @@
 package fr.framelab.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
@@ -11,7 +12,24 @@ public class EditorController {
     @FXML
     private ImageView editedImage;
 
+    @FXML
+    private ScrollPane leftScrollPane;
+
+    @FXML
+    private ScrollPane rightScrollPane;
+
     private MainController mainController;
+
+    @FXML
+    public void initialize() {
+        // Lier la taille de l'ImageView à celle du ScrollPane
+        baseImage.fitWidthProperty().bind(leftScrollPane.widthProperty());
+        baseImage.fitHeightProperty().bind(leftScrollPane.heightProperty());
+
+        // Lier la taille de l'ImageView à celle du ScrollPane
+        editedImage.fitWidthProperty().bind(rightScrollPane.widthProperty());
+        editedImage.fitHeightProperty().bind(rightScrollPane.heightProperty());
+    }
 
     public void setMainController(MainController mainController) {
         this.mainController = mainController;

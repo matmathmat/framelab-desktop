961c8b2005380cb5d1e60b35a519c46ac6ab8447
matmathmat <232357120+matmathmat@users.noreply.github.com>
2026-02-11 15:00:54 +0100

    Ajout de top bar et left bar
    - ajout top bar et left bar
    - Rend visible top et left bar uniquement si l'home est visible

--- FrameLabDesktop/src/main/java/fr/framelab/controller/HomeController.java
+++ FrameLabDesktop/src/main/java/fr/framelab/controller/HomeController.java
@@ -2,7 +2,7 @@ package fr.framelab.controller;
 
 public class HomeController {
     private MainController mainController;
-    
+
     public void setMainController(MainController mainController) {
         this.mainController = mainController;
     }
--- FrameLabDesktop/src/main/java/fr/framelab/controller/MainController.java
+++ FrameLabDesktop/src/main/java/fr/framelab/controller/MainController.java
@@ -1,10 +1,14 @@
 package fr.framelab.controller;
 
 import fr.framelab.api.FrameLabAPI;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 
 import java.io.IOException;
 
@@ -12,9 +16,21 @@ public class MainController {
     @FXML
     private StackPane contentPane;
 
+    @FXML
+    private HBox topBar;
+
+    @FXML
+    private VBox leftBar;
+
+    private final BooleanProperty homeVisible = new SimpleBooleanProperty(false);
+
     public FrameLabAPI frameLabAPI;
 
     public void initialize() {
+        // Rend visible top et left bar uniquement si l'home est visible
+        topBar.visibleProperty().bind(homeVisible);
+        leftBar.visibleProperty().bind(homeVisible);
+
         showLogin();
     }
 
@@ -38,6 +54,8 @@ public class MainController {
 
     public void showHome() {
         try {
+            homeVisible.set(true);
+
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/fr/framelab/view/home.fxml"));
             Parent view = loader.load();
 

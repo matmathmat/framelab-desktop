28ca530391011d0fb3eba6b7c2dd4210da5802c9
matmathmat <232357120+matmathmat@users.noreply.github.com>
2026-02-11 12:01:37 +0100

    Initialisation de javafx

---
+++ FrameLabDesktop/src/main/java/fr/framelab/FrameLabDesktop.java
@@ -0,0 +1,38 @@
+package fr.framelab;
+
+import fr.framelab.api.FrameLabAPI;
+import fr.framelab.controller.MainController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class FrameLabDesktop extends Application {
+    private FrameLabAPI frameLabAPI;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        this.frameLabAPI = new FrameLabAPI("localhost:3000", false);
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fr/framelab/view/main-view.fxml"));
+        BorderPane root = loader.load();
+
+        MainController mainController = loader.getController();
+        //mainController.setServices(flashCardService, deckService);
+
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("FrameLabDesktop");
+        primaryStage.show();
+    }
+
+    @Override
+    public void stop() throws Exception {
+        super.stop();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ FrameLabDesktop/src/main/java/fr/framelab/controller/MainController.java
@@ -0,0 +1,14 @@
+package fr.framelab.controller;
+
+public class MainController {
+//    @FXML
+//    private FlashCardListController flashCardListController;
+//
+//    @FXML
+//    private DeckListController deckListController;
+//
+//    public void setServices(FlashCardService flashCardService, DeckService deckService) {
+//        this.flashCardListController.setService(flashCardService);
+//        this.deckListController.setServices(deckService, flashCardService);
+//    }
+}

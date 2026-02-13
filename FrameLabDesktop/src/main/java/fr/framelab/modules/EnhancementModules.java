ad0ac687140a2540543662d1d858e9368e029810
matmathmat <232357120+matmathmat@users.noreply.github.com>
2026-02-13 11:31:15 +0100

    Améliore EnhancementModules
    On ne référence plus et on crée directement l'objet controller pour pouvoir passer editorController

--- FrameLabDesktop/src/main/java/fr/framelab/modules/EnhancementModules.java
+++ FrameLabDesktop/src/main/java/fr/framelab/modules/EnhancementModules.java
@@ -1,5 +1,6 @@
 package fr.framelab.modules;
 
+import fr.framelab.controller.EditorController;
 import fr.framelab.controller.editor.enhancement.ContrastController;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -7,18 +8,24 @@ import javafx.collections.ObservableList;
 import fr.framelab.controller.editor.enhancement.BrightnessController;
 
 public class EnhancementModules {
-    public static ObservableList<EditorModule> getModules() {
+    public static ObservableList<EditorModule> getModules(EditorController editorController) {
 
         return FXCollections.observableArrayList(
                 new EditorModule(
                         "Luminosité",
                         "Ajuster la luminosité de l'image",
-                        new BrightnessController()::show
+                        () -> {
+                            BrightnessController controller = new BrightnessController();
+                            controller.show(editorController);
+                        }
                 ),
                 new EditorModule(
                         "Contraste",
                         "Modifier le contraste",
-                        new ContrastController()::show
+                        () -> {
+                            ContrastController controller = new ContrastController();
+                            controller.show();
+                        }
                 )
         );
     }

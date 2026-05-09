b2a0bafa3bb0bf8164147ea4c32da9d3d37c53ed
matmathmat <232357120+matmathmat@users.noreply.github.com>
2026-05-09 14:04:56 +0200

    Ajout operation emoji

---
+++ FrameLabDesktop/src/main/java/fr/framelab/modules/image/EmojiOperation.java
@@ -0,0 +1,47 @@
+package fr.framelab.modules.image;
+
+import javafx.scene.SnapshotParameters;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.WritableImage;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+
+public class EmojiOperation implements ImageOperation {
+    private final String emoji;
+    private final int x;
+    private final int y;
+    private final double size;
+
+    public EmojiOperation(String emoji, int x, int y, double size) {
+        this.emoji = emoji;
+        this.x = x;
+        this.y = y;
+        this.size = size;
+    }
+
+    @Override
+    public void handle(WritableImage image) {
+        Canvas canvas = new Canvas(image.getWidth(), image.getHeight());
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+
+        // On redessine l'image de base
+        gc.drawImage(image, 0, 0);
+
+        // On configure et on pose l'emoji
+        gc.setFont(Font.font("Arial", size));
+
+        // On centre le texte sur les coordonnées cliquées
+        gc.fillText(emoji, x - (size / 3.0), y + (size / 3.0));
+
+        // On fusionne le résultat dans l'image
+        SnapshotParameters params = new SnapshotParameters();
+        params.setFill(Color.TRANSPARENT);
+        canvas.snapshot(params, image);
+    }
+
+    @Override
+    public String getName() {
+        return "Ajout Emoji : " + emoji;
+    }
+}
